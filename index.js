8e3131b79a5d9fabb59c78736f06706f6946c758
Posen992 <59381541+Posen992@users.noreply.github.com>
2024-07-28 01:47:29 +0300

    Fix warning of ESlint

--- index.js
+++ index.js
@@ -5,108 +5,137 @@ const cors = require('cors')
 require('dotenv').config()
 const Person = require('./models/person')
 
-
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 
-morgan.token('body', function (req) { return req.body })
-app.use(morgan(function (tokens, req, res) {
-	return [
-		tokens.method(req, res),
-		tokens.url(req, res),
-		tokens.status(req, res),
-		tokens.res(req, res, 'content-length'), '-',
-		tokens['response-time'](req, res), 'ms',
-		JSON.stringify(tokens['body'](req, res))
-	].join(' ')
-}))
+morgan.token('body', function (req) {
+  return req.body
+})
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'),
+      '-',
+      tokens['response-time'](req, res),
+      'ms',
+      JSON.stringify(tokens['body'](req, res)),
+    ].join(' ')
+  })
+)
+
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  res.send('ok')
+})
 
 app.get('/api/persons', (request, response, next) => {
-	Person.find({}).then(persons => {
-		response.json(persons)
-	}).catch(error => next(error))
+  Person.find({})
+    .then((persons) => {
+      response.json(persons)
+    })
+    .catch((error) => next(error))
 })
 
 app.get('/info', (request, response, next) => {
-	Person.find({}).then(persons => {
-		const currentDate = new Date()
-		const dateString = currentDate.toString()
-		response.send(`Phonebook has info for ${persons.length} people</br>${dateString}`)
-	}).catch(error => next(error))
+  Person.find({})
+    .then((persons) => {
+      const currentDate = new Date()
+      const dateString = currentDate.toString()
+      response.send(
+        `Phonebook has info for ${persons.length} people</br>${dateString}`
+      )
+    })
+    .catch((error) => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-	Person.findById(request.params.id).then(person => {
-		if (person) {
-			response.json(person)
-		} else {
-			response.status(404).send(`Infomation of ${request.params.id} has already been removed from server`)
-		}
-	}).catch(error => next(error))
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person)
+      } else {
+        response
+          .status(404)
+          .send(
+            `Infomation of ${request.params.id} has already been removed from server`
+          )
+      }
+    })
+    .catch((error) => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-	Person.findByIdAndDelete(request.params.id)
-		.then(() => {
-			response.status(204).end()
-		}).catch(error => next(error))
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
-	const body = request.body
+  const body = request.body
 
-	const person = new Person({
-		name: body.name,
-		number: body.number
-	})
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
 
-	person.save().then(savedPerson => {
-		response.json(savedPerson)
-	}).catch(error => next(error))
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson)
+    })
+    .catch((error) => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-	const body = request.body
+  const body = request.body
 
-	const person = {
-		name: body.name,
-		number: body.number
-	}
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
 
-	Person.findByIdAndUpdate(body.id, person, { runValidators: true, new: true })
-		.then(updatedPerson => {
-			if (updatedPerson){
-				response.json(updatedPerson)
-			}
-			else{
-				response.status(404).send(`Infomation of ${person.name} has already been removed from server, please refresh the page`)
-			}
-		}).catch(error => next(error))
+  Person.findByIdAndUpdate(body.id, person, { runValidators: true, new: true })
+    .then((updatedPerson) => {
+      if (updatedPerson) {
+        response.json(updatedPerson)
+      } else {
+        response
+          .status(404)
+          .send(
+            `Infomation of ${person.name} has already been removed from server, please refresh the page`
+          )
+      }
+    })
+    .catch((error) => next(error))
 })
 
-
 const unknownEndpoint = (request, response) => {
-	response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-	console.error(error.message)
+  console.error(error.message)
 
-	if (error.name === 'CastError') {
-		return response.status(400).send({ error: 'malformatted id' })
-	}else if (error.name === 'ValidationError') {
-		return response.status(400).json({ error: error.message })
-	}
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
-	next(error)
+  next(error)
 }
 
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- mongo.js
+++ mongo.js
@@ -1,45 +1,42 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-	console.log('give password as argument')
-	process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
 const newName = process.argv[3]
 const newNumber = process.argv[4]
 
-
-const url =
-    `mongodb+srv://Posen992:${password}@phonebook-database.cjdl9fv.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+const url = `mongodb+srv://Posen992:${password}@phonebook-database.cjdl9fv.mongodb.net/phonebookApp?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-	name : String,
-	number : String,
+  name: String,
+  number: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
 
-if (process.argv.length === 5){
-	const person = new Person({
-		name : newName,
-		number : newNumber,
-	})
+if (process.argv.length === 5) {
+  const person = new Person({
+    name: newName,
+    number: newNumber,
+  })
 
-	person.save().then(() => {
-		console.log('note saved!')
-		mongoose.connection.close()
-	})
-
-}else{
-	Person.find({}).then(result => {
-		console.log('phonebook:')
-		result.forEach(element => {
-			console.log(element.name,element.number)
-		})
-		mongoose.connection.close()
-	})
+  person.save().then(() => {
+    console.log('note saved!')
+    mongoose.connection.close()
+  })
+} else {
+  Person.find({}).then((result) => {
+    console.log('phonebook:')
+    result.forEach((element) => {
+      console.log(element.name, element.number)
+    })
+    mongoose.connection.close()
+  })
 }
